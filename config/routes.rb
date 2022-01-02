7d48e5251c8f83b5fca17a68e7e884035fa84551
silasdani <danielsilas23@yahoo.com>
2022-01-02 20:30:18 +0200

    added devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             path: 'users',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               password: 'secret',
+               confirmation: 'verification',
+               unlock: 'unblock',
+               registration: 'register',
+               sign_up: 'cmon_let_me_in'
+             }
 end
